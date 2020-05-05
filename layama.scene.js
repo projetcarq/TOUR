50545419dca3088bd241b3b59fa6cac123e86965
tymy123 <36450709+tymy123@users.noreply.github.com>
2020-05-05 14:16:00 -0500

    TOUR

---
+++ layama.scene.js
@@ -0,0 +1,145 @@
+// Created with Motiva Layama www.motivacg.com
+
+function getLayamaCameraPositions()
+{
+   var cameraPositions = new BABYLON.SmartArray(0);
+   cameraPositions.push(new BABYLON.Vector3(47.0179, 141.829, -905.321));
+   cameraPositions.push(new BABYLON.Vector3(147.361, 148.802, -852.438));
+   cameraPositions.push(new BABYLON.Vector3(-17.0672, 148.802, -854.471));
+   cameraPositions.push(new BABYLON.Vector3(-0.414032, 148.802, -712.758));
+   cameraPositions.push(new BABYLON.Vector3(135.6, 148.802, -705.16));
+   cameraPositions.push(new BABYLON.Vector3(-64.9685, 148.802, -559.497));
+   cameraPositions.push(new BABYLON.Vector3(-65.596, 148.802, -503.404));
+   cameraPositions.push(new BABYLON.Vector3(-66.2297, 148.802, -206.156));
+   cameraPositions.push(new BABYLON.Vector3(-67.8879, 148.802, -106.255));
+   cameraPositions.push(new BABYLON.Vector3(-65.6003, 148.802, 6.98523));
+   cameraPositions.push(new BABYLON.Vector3(-73.0352, 148.802, 81.9066));
+   cameraPositions.push(new BABYLON.Vector3(-83.3297, 148.802, 220.883));
+   cameraPositions.push(new BABYLON.Vector3(-75.8948, 148.802, 176.273));
+   cameraPositions.push(new BABYLON.Vector3(-13.5383, 148.802, 182.029));
+   cameraPositions.push(new BABYLON.Vector3(60.0105, 148.802, 247.903));
+   cameraPositions.push(new BABYLON.Vector3(130.04, 148.802, 166.719));
+   cameraPositions.push(new BABYLON.Vector3(135.927, 148.802, 249.453));
+   cameraPositions.push(new BABYLON.Vector3(206.266, 148.802, 158.353));
+   cameraPositions.push(new BABYLON.Vector3(42.8838, 148.802, 125.976));
+   cameraPositions.push(new BABYLON.Vector3(42.8838, 148.802, 84.2022));
+   cameraPositions.push(new BABYLON.Vector3(35.8978, 148.802, -28.4473));
+   cameraPositions.push(new BABYLON.Vector3(38.5176, 148.802, -150.703));
+   cameraPositions.push(new BABYLON.Vector3(98.1202, 148.802, -227.149));
+   cameraPositions.push(new BABYLON.Vector3(171.327, 148.802, -224.558));
+   cameraPositions.push(new BABYLON.Vector3(228.986, 148.802, -224.558));
+   cameraPositions.push(new BABYLON.Vector3(120.147, 148.802, 40.4142));
+   cameraPositions.push(new BABYLON.Vector3(192.707, 148.802, 35.8793));
+   cameraPositions.push(new BABYLON.Vector3(244.535, 148.802, 37.8229));
+   cameraPositions.push(new BABYLON.Vector3(-8.12759, 148.802, -240.106));
+   cameraPositions.push(new BABYLON.Vector3(185.93, 148.803, 325.016));
+   cameraPositions.push(new BABYLON.Vector3(135.6, 148.802, -644.8));
+   cameraPositions.push(new BABYLON.Vector3(-0.414032, 148.802, -655.223));
+   cameraPositions.push(new BABYLON.Vector3(-56.7261, 148.802, -621.735));
+   cameraPositions.push(new BABYLON.Vector3(-65.367, 148.803, -325.556));
+   cameraPositions.push(new BABYLON.Vector3(135.6, 148.802, -587.217));
+   cameraPositions.push(new BABYLON.Vector3(135.6, 148.802, -541.783));
+   cameraPositions.push(new BABYLON.Vector3(135.6, 148.802, -500.262));
+   cameraPositions.push(new BABYLON.Vector3(135.6, 148.802, -458.056));
+   cameraPositions.push(new BABYLON.Vector3(-65.596, 148.802, -447.986));
+   return cameraPositions;
+}
+
+function getLayamaCameraLookAts()
+{
+   var cameraLookAt = new BABYLON.SmartArray(0);
+   cameraLookAt.push(new BABYLON.Vector3(46.0806, 141.83, -904.973));
+   cameraLookAt.push(new BABYLON.Vector3(147.188, 148.802, -851.453));
+   cameraLookAt.push(new BABYLON.Vector3(-16.8935, 148.802, -853.487));
+   cameraLookAt.push(new BABYLON.Vector3(0.585968, 148.802, -712.758));
+   cameraLookAt.push(new BABYLON.Vector3(136.6, 148.802, -705.16));
+   cameraLookAt.push(new BABYLON.Vector3(-63.9687, 148.813, -559.511));
+   cameraLookAt.push(new BABYLON.Vector3(-64.5961, 148.813, -503.418));
+   cameraLookAt.push(new BABYLON.Vector3(-65.2299, 148.813, -206.17));
+   cameraLookAt.push(new BABYLON.Vector3(-66.8881, 148.813, -106.268));
+   cameraLookAt.push(new BABYLON.Vector3(-64.6005, 148.813, 6.97139));
+   cameraLookAt.push(new BABYLON.Vector3(-72.0876, 148.811, 82.2259));
+   cameraLookAt.push(new BABYLON.Vector3(-83.3576, 148.805, 221.882));
+   cameraLookAt.push(new BABYLON.Vector3(-75.0435, 148.81, 176.798));
+   cameraLookAt.push(new BABYLON.Vector3(-13.9972, 148.822, 181.141));
+   cameraLookAt.push(new BABYLON.Vector3(59.0108, 148.823, 247.917));
+   cameraLookAt.push(new BABYLON.Vector3(129.04, 148.823, 166.733));
+   cameraLookAt.push(new BABYLON.Vector3(134.927, 148.823, 249.467));
+   cameraLookAt.push(new BABYLON.Vector3(205.266, 148.823, 158.367));
+   cameraLookAt.push(new BABYLON.Vector3(43.8837, 148.813, 125.962));
+   cameraLookAt.push(new BABYLON.Vector3(43.8837, 148.813, 84.1883));
+   cameraLookAt.push(new BABYLON.Vector3(36.8976, 148.813, -28.4611));
+   cameraLookAt.push(new BABYLON.Vector3(39.5175, 148.813, -150.716));
+   cameraLookAt.push(new BABYLON.Vector3(99.12, 148.813, -227.163));
+   cameraLookAt.push(new BABYLON.Vector3(172.327, 148.813, -224.572));
+   cameraLookAt.push(new BABYLON.Vector3(229.986, 148.813, -224.572));
+   cameraLookAt.push(new BABYLON.Vector3(121.147, 148.813, 40.4004));
+   cameraLookAt.push(new BABYLON.Vector3(193.707, 148.813, 35.8655));
+   cameraLookAt.push(new BABYLON.Vector3(245.535, 148.813, 37.8091));
+   cameraLookAt.push(new BABYLON.Vector3(-8.63946, 148.822, -240.965));
+   cameraLookAt.push(new BABYLON.Vector3(184.93, 148.823, 325.03));
+   cameraLookAt.push(new BABYLON.Vector3(136.6, 148.802, -644.8));
+   cameraLookAt.push(new BABYLON.Vector3(0.585968, 148.802, -655.223));
+   cameraLookAt.push(new BABYLON.Vector3(-55.7261, 148.802, -621.735));
+   cameraLookAt.push(new BABYLON.Vector3(-64.3671, 148.813, -325.57));
+   cameraLookAt.push(new BABYLON.Vector3(136.6, 148.802, -587.217));
+   cameraLookAt.push(new BABYLON.Vector3(136.6, 148.802, -541.783));
+   cameraLookAt.push(new BABYLON.Vector3(136.6, 148.802, -500.262));
+   cameraLookAt.push(new BABYLON.Vector3(136.6, 148.802, -458.056));
+   cameraLookAt.push(new BABYLON.Vector3(-64.5961, 148.813, -448));
+   return cameraLookAt;
+}
+
+function getLayamaCubeTextures()
+{
+   var cameraTextureNames = new BABYLON.SmartArray(0);
+   cameraTextureNames.push("Camerainicio_Beauty");
+   cameraTextureNames.push("CameraActor2_3_Beauty");
+   cameraTextureNames.push("CameraActor3_5_Beauty");
+   cameraTextureNames.push("CameraActor4_7_Beauty");
+   cameraTextureNames.push("CameraActor5_9_Beauty");
+   cameraTextureNames.push("CameraActor8_Beauty");
+   cameraTextureNames.push("CameraActor10_Beauty");
+   cameraTextureNames.push("CameraActor11_Beauty");
+   cameraTextureNames.push("CameraActor12_Beauty");
+   cameraTextureNames.push("CameraActor13_Beauty");
+   cameraTextureNames.push("CameraActor14_Beauty");
+   cameraTextureNames.push("CameraActor15_Beauty");
+   cameraTextureNames.push("CameraActor16_Beauty");
+   cameraTextureNames.push("CameraActor17_Beauty");
+   cameraTextureNames.push("CameraActor18_Beauty");
+   cameraTextureNames.push("CameraActor19_Beauty");
+   cameraTextureNames.push("CameraActor20_Beauty");
+   cameraTextureNames.push("CameraActor21_Beauty");
+   cameraTextureNames.push("CameraActor22_Beauty");
+   cameraTextureNames.push("CameraActor23_Beauty");
+   cameraTextureNames.push("CameraActor24_Beauty");
+   cameraTextureNames.push("CameraActor25_Beauty");
+   cameraTextureNames.push("CameraActor26_Beauty");
+   cameraTextureNames.push("CameraActor27_Beauty");
+   cameraTextureNames.push("CameraActor28_Beauty");
+   cameraTextureNames.push("CameraActor29_Beauty");
+   cameraTextureNames.push("CameraActor30_Beauty");
+   cameraTextureNames.push("CameraActor31_Beauty");
+   cameraTextureNames.push("CameraActor32_Beauty");
+   cameraTextureNames.push("CameraActor33_Beauty");
+   cameraTextureNames.push("CameraActor34_Beauty");
+   cameraTextureNames.push("CameraActor35_Beauty");
+   cameraTextureNames.push("CameraActor36_Beauty");
+   cameraTextureNames.push("CameraActor37_Beauty");
+   cameraTextureNames.push("CameraActor38_Beauty");
+   cameraTextureNames.push("CameraActor39_Beauty");
+   cameraTextureNames.push("CameraActor40_Beauty");
+   cameraTextureNames.push("CameraActor41_Beauty");
+   cameraTextureNames.push("CameraActor42_Beauty");
+   return cameraTextureNames;
+}
+
+function getLayamaResolutions()
+{
+   var layamaResolutions = new BABYLON.SmartArray(0);
+   layamaResolutions.push("2048");
+   layamaResolutions.push("1024");
+   return layamaResolutions;
+}
+
